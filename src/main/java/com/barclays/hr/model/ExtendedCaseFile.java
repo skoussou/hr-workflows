b6deb68af2ce4430013ade693c7875604e0aa74b
Ally Jarrett <ajarrett@redhat.com>
2018-07-19 09:53:53 +0100

    Advanced Query deployment

---
+++ src/main/java/com/barclays/hr/model/ExtendedCaseFile.java
@@ -0,0 +1,19 @@
+package com.barclays.hr.model;
+
+import java.io.Serializable;
+
+@org.kie.api.definition.type.Label("ExtendedCaseFile")
+public class ExtendedCaseFile extends org.kie.server.api.model.cases.CaseInstance implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private String caseOwner;
+
+  public String getCaseOwner() {
+    return caseOwner;
+  }
+  public void setCaseOwner(String caseOwner) {
+      this.caseOwner = caseOwner;
+  }
+  
+}
